aedcbd3bd648ff211be9c6fe761e9cb93d40cedc
mrodriguezzsx <mrodrigu@jaumebalmes.net>
2023-06-11 11:57:54 +0200

    Afegir Productes Format Json

--- Botiga_grupA/Botiga_grupA/urls.py
+++ Botiga_grupA/Botiga_grupA/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tienda/catalogo', include('cataleg.urls'))
+    path('tienda/catalogo/', include('cataleg.urls'))
 ]
